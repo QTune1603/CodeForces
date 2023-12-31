49849267300dcb599b00ee869aa4864390667ee8
QTune <tungonlytop1@gmail.com>
2023-12-31 21:52:38 +0700

    Update 4A.cpp

--- 4A.cpp
+++ 4A.cpp
@@ -1,29 +1,17 @@
 #include <iostream>
 #include <string>
 #include <cmath>
-
+ 
 using namespace std;
-
-
+ 
+ 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-
-    unsigned long long w; cin >> w;
-    if(w % 2 != 0){
-        cout << "NO";
-        return;
+ 
+    int w; cin >> w;
+    if(w % 2 == 0 && w > 2){
+        cout << "YES" << endl;
     }
-    int cnt = 0;
-    for(int i = 2; i <= w ; i+=2){
-        for(int j = w ; j >= 2 ; j-=2){
-            if(i + j == w){
-                cnt = 1;
-                break;
-            }
-        }
-    }
-    if(cnt == 1) {
-        cout << "YES";
-    }
+    else cout << "NO" << endl;
 }
