3b073f8b4c68da7567c8627c1e81b4f2db1bbae7
QTune <tungonlytop1@gmail.com>
2024-01-03 01:15:24 +0700

    Create 50A.cpp

---
+++ 50A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int m,n; cin >> m >> n;
+    cout << (m*n)/2;
+}
